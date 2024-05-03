5265a89e311470d61420cf27c60764f2f558a2d9
Baranidaran082 <baranidaran08@gmail.com>
2024-05-03 16:33:07 +0530

    adding again

---
+++ tuple.py
@@ -0,0 +1,19 @@
+tuple1 = ("red","green","yellow")
+# tuple1[1] = "black"
+# tuple1.append("orange")
+# print(tuple1)
+
+# y = list(tuple1)
+# y.append("black")
+# tuple1 = tuple(y)
+# print(tuple1)
+
+
+#adding two tuples
+# y = ("black","grey")
+# tuple1 = tuple1 + y
+# print(tuple1)
+
+# #delete
+# del tuple1
+# print(tuple1)
